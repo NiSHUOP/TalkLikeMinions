0971b83a6f6229860034673cd8a6c4581b7b2ed4
Nishu Anand <nishuanand123gooda@gmail.com>
2023-05-20 14:00:22 +0530

    fix: api bug

--- script.js
+++ script.js
@@ -2,32 +2,26 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var textOutput = document.querySelector("#text-output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text) {
-  return serverURL + "?" + "text" + text
+  return serverURL + "?text=" + text;
 }
 
-// querySelector use for select that particular id
-// addEventListener is use for adding task that we performed by the id
+function errorHandler(error) {
+    alert("Something wrong with the server, Try again later.");  // Error handling function 
+  }
 
-// btnTranslate.addEventListener("click",function clickEventHandler() {
-//   console.log("clicked!")
-// })
 function clickHandler(){
-  // console.log("clicked!");
-  // console.log("input",textInput.value);
-  // textOutput.innerText=textInput.value;
 
   var inputText = textInput.value;  //taking input
 
   // calling server for processing
   fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => {
-      textOutput.innerText = json.contents.translated;
-    })
+    .then(json => textOutput.innerText = json.contents.translated
+    )
+    .catch(errorHandler);
 };
 
-btnTranslate.addEventListener("click",clickHandler)
-// textOutput.innerText="Nishu";
+btnTranslate.addEventListener("click",clickHandler);
